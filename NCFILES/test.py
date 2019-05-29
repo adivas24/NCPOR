41c76e3ed2c014b47b3c63972b1f7d92f6c397c9
Aditya Vasudevan <f20170175@pilani.bits-pilani.ac.in>
2019-05-29 09:52:49 +0530

    Committing all work done on 28/01/2019

--- NCFILES/test.py
+++ NCFILES/test.py
@@ -4,21 +4,18 @@ from netCDF4 import Dataset
 import sys
 import Tkinter as tk
 import math
+from datetime import datetime, timedelta
+from netCDF4 import num2date, date2num
+import pandas as pd
 
 num_files = len(sys.argv)-1
 nc_array = []
 for i in range(num_files):
 	nc_array.append(Dataset(sys.argv[i+1], 'r'))
 
-#print nc_array[0].variables['u10'].dimensions
-dim_no = 0
+dim_no = 1
 dim_set = [x for x in nc_array[0].dimensions.keys() if x != u'time' and x != u'Time']
 dime_info = dict()
-time_range = []
-time_unit = nc_array[0].variables['time'].units.split()
-if (time_unit[0] == "hours" and time_unit[1] == "since"):
-	year_base = int(time_unit[2].split('-')[0])
-
 for a in dim_set:
 	array = [None, None, None]
 	array[0] = nc_array[0].variables[a][:]
@@ -27,23 +24,33 @@ for a in dim_set:
 	dime_info[a] = array
 	dim_no += 1
 
+
+time_range = []
 for x in nc_array:
 	time_range = np.append(time_range, x.variables['time'][:])
-year_min = int((np.amin(time_range)//(24*365)))+year_base - int(np.amin(time_range)//(24*365*365*4))
-year_max = int((np.amax(time_range)//(24*365)))+year_base - int(np.amin(time_range)//(24*365*365*4))
+
+
+time_unit = nc_array[0].variables['time'].units
+try:
+	time_calendar = nc_array[0].variables['time'].time_calendar
+except:
+	time_calendar = "standard"
+
+time_array = [num2date(a,time_unit,time_calendar) for a in time_range]
+time_array_2 = [a.strftime("%m")+"/"+a.strftime("%Y") for a in time_array]
+
+year_min = int(min(time_array).strftime("%Y"))
+year_max = int(max(time_array).strftime("%Y"))
 
 
 vars_array =[x for x in nc_array[0].variables.keys() if x not in dim_set and x != u'time' and x!= u'Time']
 var_data_array = dict()
 for i in range(num_files):
 	for a in vars_array:
-		var_data_array[a] = ma.concatenate([var_data_array.get(a,ma.empty(nc_array[i].variables[a][:].shape)),nc_array[i].variables[a][:]], 0)
-
-dataset = []
-months = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec']
-for b in range(year_min,year_max+1):
-	for a in months:
-		dataset.append(a+' '+ str(b))
+		if (a in var_data_array.keys()):
+			var_data_array[a] = ma.concatenate([var_data_array[a],nc_array[i].variables[a][:]],0)
+		else:
+			var_data_array[a] = nc_array[i].variables[a][:]
 
 def trig(event,b,c):
 	chk_i = int(event[4])		#This will fail if number of dimensions is more than a single digit number
@@ -68,15 +75,15 @@ def getData(msg1, msg2):
 			index_vars[i] = np.argmin(np.abs(dime_info[i][0]-float(spinbox_array[x*2].get())))
 			x+=1
 		year = spinbox_array[2*len(dim_set)].get()
-		month = months.index(year[0:3])
-		yr = int(year[3:8])
-		index_vars['time'] = month + (yr-year_min)*12
+		index_vars['time'] = np.argmin(np.abs(time_range-date2num(datetime(int(year[3:7]),int(year[0:2]),1),time_unit, time_calendar)))
+	
 		message1 = 'You have selected\n'
 		message2 = ''
+	
 		for x in dim_set:
 			message1 = message1 + x + ': ' + str(dime_info[x][0][index_vars[x]])+'\n'
-		message1 = message1 + 'time: ' + year
-
+		message1 = message1 + 'time: ' + time_array_2[index_vars['time']]
+	
 		for y in vars_array:
 			ind_array = tuple([np.array(index_vars[a]) for a in nc_array[0].variables[y].dimensions])
 			message2 = message2 + y +': ' + str(var_data_array[y][ind_array]) +'\n'
@@ -90,29 +97,28 @@ def getData(msg1, msg2):
 			x += 1
 		
 		year1 = spinbox_array[2*len(dim_set)].get()
-		month1 = months.index(year1[0:3])
-		yr1 = int(year1[3:8])
-		index_vars['time'] = [month1 + (yr1-year_min)*12,month1 + (yr1-year_min)*12 +1] 
+		index_vars['time'] = [np.argmin(np.abs(time_range-date2num(datetime(int(year1[3:7]),int(year1[0:2]),1),time_unit, time_calendar))) for i in range(2)]
+		index_vars['time'][1] += 1
+
 		if (chk_var[x].get() == 1):
 			year2 = spinbox_array[len(dim_set)*2+1].get()
-			month2 = months.index(year2[0:3])
-			yr2 = int(year2[3:8])
-			index_vars['time'][1] = month2 + (yr2-year_min)*12
-
+			index_vars['time'][1] = np.argmin(np.abs(time_range-date2num(datetime(int(year2[3:7]),int(year2[0:2]),1),time_unit, time_calendar)))
+	
 		message1 = 'You have selected:\n'
 		message2 = ''
 		for x in dim_set:
 			message1 = message1 + x +': ' +str(dime_info[x][0][index_vars[x][0]]) + ':' + str(dime_info[x][0][index_vars[x][1]])+ '\n'
-		message1 = message1 + 'time: ' + year1 +':' + year2
+		message1 = message1 + 'time: ' + time_array_2[index_vars['time'][0]] +':' + time_array_2[index_vars['time'][1]]
 		for y in vars_array:
 			ind_array =tuple([slice(min(index_vars[a][0],index_vars[a][1]),max(index_vars[a][0],index_vars[a][1])+1) for a in nc_array[0].variables[y].dimensions])
-			print ind_array
+			#print ind_array
 			message2 = message2 + y+ ': ' + str(var_data_array[y][ind_array]) + '\n'
 
 	msg1.delete(1.0,tk.END)
 	msg2.delete(1.0,tk.END)
 	msg1.insert(tk.INSERT, message1)
 	msg2.insert(tk.INSERT, message2)
+	#pd.DataFrame(var_data_array['u10'][ind_array]).to_csv("testcsvfile.csv")
 
 global m, textBox1,textBox2, chk_var
 
@@ -132,9 +138,9 @@ for i in dim_set:
 	r2 = tk.Radiobutton(m, text = 'Range', variable = chk_var[n1], value = 1)
 	r2.grid(row = n1, column = 2)
 	r2.deselect()
-	tk.Label(m, text = i).grid(row = n1+3,column = 0)
-	spinbox_array[2*n1].configure(from_ = dime_info[i][1], to_ = dime_info[i][2], increment = 0.75, format = '%5.2f')
-	spinbox_array[2*n1 + 1].configure(from_ = dime_info[i][1], to_ = dime_info[i][2], increment = 0.75, format = '%5.2f')
+	tk.Label(m, text = i).grid(row = n1+dim_no,column = 0)
+	spinbox_array[2*n1].configure(values = tuple(dime_info[i][0]), format = '%5.2f')
+	spinbox_array[2*n1 + 1].configure(values = tuple(dime_info[i][0]), format = '%5.2f')
 	n1+=1
 
 
@@ -147,11 +153,11 @@ r2.grid(row = n1, column = 2)
 r2.deselect()
 for i in chk_var:
 	i.trace("w", trig)
-tk.Label(m, text = 'Time').grid(row = n1+3,column = 0)
-spinbox_array[2*n1].configure(values=dataset)
-spinbox_array[2*n1+1].configure(values = dataset)
+tk.Label(m, text = 'Time').grid(row = n1+dim_no,column = 0)
+spinbox_array[2*n1].configure(values=time_array_2)
+spinbox_array[2*n1+1].configure(values = time_array_2)
 
-n1 += 4
+n1 += 1+dim_no
 textBox1 = tk.Text(m, height = 4)
 textBox1.grid(row = n1, column = 0, columnspan = 10)
 scroll1 = tk.Scrollbar(m)
