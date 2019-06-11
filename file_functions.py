ec07c24356fa6cd7d3704dee4e8bd6cf58145cda
Aditya Vasudevan <f20170175@pilani.bits-pilani.ac.in>
2019-06-11 11:58:07 +0530

    Added documentation to the file_functions.py Cleaning hasn't been done, only comments have been added at this point.

--- file_functions.py
+++ file_functions.py
@@ -1,20 +1,36 @@
-import xarray as xr
+# file_functions.py #
+
 import gl_vars
-import cartopy.crs as ccrs
-import matplotlib.pyplot as plt
+
+import xarray as xr
+# Used for manipulation of netCDF files and the data within them.
+import numpy as np
+# Used for numerical calculations.
+
+import pandas as pd
 import geopandas as gpd
+# Used for some data manipulation
+
 import fiona
 import shapely.geometry as sgeom
-import numpy as np
-import pandas as pd
 from rasterio import features
 from affine import Affine
-import re
+# Used for SHAPEFILES
 
+# TODO: Merge and rearrange the content of these functions, there seems to be a lot of redundancy.
+#		Correct a few errors speciifically mentioned.
+#		Exception, handling and fool-proofing.
 
+# PRE-CONDITION
+#	filenames: A list of strings corresponding to each NETCDF file containing the full path of the file.
 def openNETCDF(filenames):
 	return [xr.open_dataset(i) for i in filenames]
+# POST-CONDITION
+#	return value: an array containing xarray DataFrames, each corresponding to one NETCDF file.
 
+# PRE-CONDITION
+#	ind: The index of the page, represents the NETCDF file being queried as an integer.
+#	gl_vars.data, gl_vars.outVar, and gl_vars.messages must be initialized before function call.
 def getData(ind):
 	dimension_list = list(gl_vars.data[ind].coords.keys())
 	variable_list = list(gl_vars.data[ind].data_vars.keys())
@@ -46,7 +62,13 @@ def getData(ind):
 			output_message += x + '\n' + str(temp) +'\nMean: '+str(np.nanmean(temp))+' Standard Deviation: '+str(np.nanstd(temp))+'\n'
 		j += 1
 	return sel_message, output_message
+# POST-CONDITION
+#	return values: A 2-tuple containing the ranges/values selected by the user and the output data in the form of strings. 
 
+# PRE-CONDITION
+#	ind: The index of the page, represents the NETDCF file being queried as an integer.
+#	var_name: The name of the variable being queried, in the form of a string.
+#	gl_vars.data and gl_vars.messages must be initialized before function call.
 def getData2(ind, var_name):
 	dimension_list = list(gl_vars.data[ind].coords.keys())
 	mess_ind = [[None, None] for i in dimension_list]
@@ -68,9 +90,14 @@ def getData2(ind, var_name):
 		j += 1
 	ord_arr = [dimension_list.index(a) for a in gl_vars.data[ind].variables[var_name].dims]
 	out_ind = tuple([mess_ind_2[a] for a in ord_arr])
-
 	return sel_message, gl_vars.data[ind].variables[var_name].values[out_ind]
+# POST-CONDITION
+#	return values: A string containing the selected value ranges and the output, but in the form of an numpy array as a 2-tuple.
 
+# PRE-CONDITION
+#	i: The index of the page, represents the NETDCF file being queried as an integer.
+#	masked_data: The masked data after applying the SHAPEFILE restrictions. It is a dictioinary of a list containing xarray DataArrays.
+#	gl_vars.data, gl_vars.outVar, and gl_vars.messages must be initialized before function call.
 def getData3(i, masked_data):
 	dimension_list = list(gl_vars.data[i].coords.keys())
 	variable_list = list(masked_data.keys())
@@ -101,9 +128,17 @@ def getData3(i, masked_data):
 			temp = np.array(masked_data[x])[out_ind]
 			output_message += x + '\n' + str(temp) +'\nMean: '+str(np.nanmean(temp))+' Standard Deviation: '+str(np.nanstd(temp))+'\n'
 		j += 1
-	#print(sel_message, output_message)
 	return sel_message, output_message
+# POST-CONDITION
+#	return values: A 2-tuple containing the ranges/values selected by the user and the output data in the form of strings. 
 
+# PRE-CONDITION
+#	ind: The index of the page, represents the NETDCF file being queried as an integer.
+#	var_name: The name of the variable being queried, in the form of a string. If None, it indicates all variables slected should be considered.
+#	time_index: The index (as an integer) of the time point selected, at which the shape is being generated. If None, data across the entire range is returned as a list.
+#	shpfile: A string containing the name and path of the SHAPEFILE(.shp) being used.
+#	plac_ind: As of now, it is a single integer containing the index of the place (specific geometry) selected by the user from the SHAPEFILE. None indicates the entire file being used. In the future a list should be passed, which can accurately select multiple but not all the places present. Maybe through a clickable-map interface.
+#	gl_vars.data should be initialized before the function call.
 def getShapeData(ind,var_name, time_index, shpfile, plac_ind):
 	
 	xds = gl_vars.data[ind]
@@ -164,3 +199,6 @@ def getShapeData(ind,var_name, time_index, shpfile, plac_ind):
 				out_arr.append(da_1.where(~np.isnan(da_1[a]),other = np.nan).values)
 			fin_arr[a] = out_arr
 		return fin_arr
+# POST-CONDITION
+#	return value: If both var_name and time_index are supplied as None, the function will return a dictionary where keys are the variables and each value is a list of xarray dataArrays corresponding to each time point, after masking the data using the SHAPEFILE geometry.
+#		Else, it returns the specific (for that variable, at that time) masked xarray DataArray.
